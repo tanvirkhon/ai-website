0ccb9c9d35fa714a69df89aef5d5ae3ceefacf61
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-04-23 23:42:36 -0500

    Added hero section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+// Import components
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+
+// Import styles
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <div className='main'>
+        <div className='gradient'></div>
+      </div>
+
+      <div className='app'>
+        <Hero />
+        <Demo />
+      </div>
+    </main>
   )
 }
 
